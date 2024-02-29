68f1a8cf7fa054f9f9d2ca0730d310950e2957ff
mubarkkhan <ahad771418@gmail.com>
2024-02-29 14:17:13 +0530

    first commit

--- src/screencomponentes/edit.jsx
+++ src/screencomponentes/edit.jsx
@@ -1,57 +1,59 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
 
-function Edit(){
+function Edit() {
     const [id] = useSearchParams()
     const myid = id.get('id')
     const [name, setname] = useState('')
     const [status, setstatus] = useState('')
-    const [user,setuser] = useState('')
-    const getedit = async()=>{
-        const res = await fetch(`http://localhost:8000/editget${myid}`,{
-            method : 'GET',
-            headers : {
-                'Content-Type' : 'application/json'
-            }
-        })
-        const update = await res.json()
-        setuser(update)
-    }
-    useEffect(()=>{
+    const [user, setuser] = useState('')
+    const getedit = useCallback(
+        async () => {
+            const res = await fetch(`http://localhost:8000/editget${myid}`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+            const update = await res.json()
+            setuser(update)
+        }, [myid]
+    )
+    useEffect(() => {
         getedit();
     }, [getedit]);
 
-    const updatedata = async(e)=>{
-        const res = await fetch(`http://localhost:8000/edit/${e}`,{
-            method : 'PATCH',
-            body : JSON.stringify({
-                'name' : (name.length>0 ? name : user.edit.name),
-                'status' : (status.length>0 ? status : user.edit.status)
+    const updatedata = async (e) => {
+        const res = await fetch(`http://localhost:8000/edit/${e}`, {
+            method: 'PATCH',
+            body: JSON.stringify({
+                'name': (name.length > 0 ? name : user.edit.name),
+                'status': (status.length > 0 ? status : user.edit.status)
             }),
-            headers : {
-                'Content-Type' : 'application/json'
+            headers: {
+                'Content-Type': 'application/json'
             }
         })
         const update = await res.json()
-        if(update.status === true){
+        if (update.status === true) {
             toast.success(update.message)
         }
-        else{
+        else {
             toast.error(update.message)
         }
     }
-    return(
+    return (
         <>
-        <ToastContainer/>
-        <div className="edit">
-            <form>
-                <input type="text" value={name} name="name" onChange={(e)=>{setname(e.target.value)}} placeholder={user && user.edit.name}/>
-                <input type="text" value={status} name="status" onChange={(e)=>{setstatus(e.target.value)}} placeholder={user && user.edit.status}/>
-                <button onClick={()=>{updatedata(user && user.edit._id)}}>Update</button>
-            </form>
-        </div>
+            <ToastContainer />
+            <div className="edit">
+                <form>
+                    <input type="text" value={name} name="name" onChange={(e) => { setname(e.target.value) }} placeholder={user && user.edit.name} />
+                    <input type="text" value={status} name="status" onChange={(e) => { setstatus(e.target.value) }} placeholder={user && user.edit.status} />
+                    <button onClick={() => { updatedata(user && user.edit._id) }}>Update</button>
+                </form>
+            </div>
         </>
     )
 }
-export{Edit}
+export { Edit }
